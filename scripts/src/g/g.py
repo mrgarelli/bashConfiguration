677485aa153c3f4875ecd088880fb8c38b67f8f9
Matthew <mrgarelli@gmail.com>
2020-02-01 21:47:06 -0500

    changed .xonshrc to point to SysPy not Syspy

--- scripts/src/g/g.py
+++ scripts/src/g/g.py
@@ -130,8 +130,8 @@ def add_all_platform_files():
 			'/.inputrc',
 			'/.local/share/konsole/*',
 			'/.config/konsolerc',
-			'/.config/Code/User/keybindings.json',
-			'/.config/Code/User/settings.json',
+			'/.config/Code\ -\ OSS/User/keybindings.json',
+			'/.config/Code\ -\ OSS/User/settings.json',
 			'/.config/openbox/lxde-rc.xml',
 			'/.AndroidStudio3.5/config/keymaps',
 			]
