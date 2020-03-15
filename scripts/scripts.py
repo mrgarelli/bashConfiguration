976c182b86d691a378a2b9e74e7f2180648408b5
Matthew <mrgarelli@gmail.com>
2020-03-15 15:18:26 -0400

    scripts.py can now specify invalid source files to ignore

--- scripts/scripts.py
+++ scripts/scripts.py
@@ -6,5 +6,6 @@ from syspy.implementations import source_executables
 sh = Shell()
 sh.verbose = False
 extensions = ['.sh', '.py', '.js', '.xsh']
+ignore_sources = ['__init__.py', '__pycache__', '.pytest_cache']
 
-source_executables(sh, extensions)
+source_executables(sh, extensions, ignore_sources)
